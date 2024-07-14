3a25b5bb082d6eaa37249945af3f27b00d02f6cc
Сергей Баданов <spy02m@gmail.com>
2024-07-14 21:01:06 +0400

    env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'kittygram.servequake.com', 'localhost', '130.193.41.143']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
